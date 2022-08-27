8059f4aca6375a91c646f7f23b1ff8b05f61ae19
Tracy Derraco <ecert31@gmail.com>
2022-08-27 11:52:15 -0700

    new repo for PWA

---
+++ client/src/js/database.js
@@ -0,0 +1,43 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDB = await openDB("jate", 1);
+
+  const tx = jateDB.transaction("jate", "readwrite");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.put({ value: content });
+
+  const result = await request;
+  console.log("data saved to the DB", result);
+};
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDB = await openDB("jate", 1);
+
+  const tx = jateDB.transactions("jate", "readonly");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result.value;
+};
+
+initdb();
